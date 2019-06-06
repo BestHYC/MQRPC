58029ba8cc1d898ae20d5509ba8ab2d9f2c6ec9f
hongyichao <970973742@qq.com>
2019-06-06 19:13:53 +0800

    添加mq执行活动
    Signed-off-by: hongyichao <970973742@qq.com>

---
+++ Framework/MQRPC/RequestContext.cs
@@ -0,0 +1,21 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+/*===================================================
+* 类名称: RequestContext
+* 类描述: 针对MQ传递数据初始化的信息数据
+* 创建人: hongyichao
+* 创建时间: 2019/6/6 18:21:33
+* 修改人: 洪移潮
+* 修改时间:
+* 版本： @version 1.0
+=====================================================*/
+namespace Framework.MQRPC
+{
+    public class RequestContext
+    {
+    }
+}
---
+++ Framework/MQRPC/RouteData.cs
@@ -0,0 +1,21 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+/*===================================================
+* 类名称: RouteData
+* 类描述: 针对请求过来的路由模板
+* 创建人: hongyichao
+* 创建时间: 2019/6/6 18:23:22
+* 修改人: 洪移潮
+* 修改时间:
+* 版本： @version 1.0
+=====================================================*/
+namespace Framework.MQRPC
+{
+    public class RouteData
+    {
+    }
+}
