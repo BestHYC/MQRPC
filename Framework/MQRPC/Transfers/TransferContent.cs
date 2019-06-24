c897b63ae7651475b87e776dc40f33bf51466e0c
hongyichao <970973742@qq.com>
2019-06-24 17:02:19 +0800

    增加传输Context协议
    Signed-off-by: hongyichao <970973742@qq.com>

---
+++ Framework/MQRPC/Transfers/TransferContent.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+/*===================================================
+* 类名称: TransferContent
+* 类描述: 传输的内容
+* 创建人: hongyichao
+* 创建时间: 2019/6/11 18:07:11
+* 修改人: 洪移潮
+* 修改时间:
+* 版本： @version 1.0
+=====================================================*/
+namespace Framework.MQRPC.Transfers
+{
+    public class TransferContent
+    {
+        /// <summary>
+        /// 传输内容头信息
+        /// </summary>
+        public TransferContentHeaders Headers { get; }
+    }
+}
---
+++ Framework/MQRPC/Transfers/TransferContentHeaders.cs
@@ -0,0 +1,39 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+/*===================================================
+* 类名称: TransferContentHeaders
+* 类描述: 传输的协议头
+* 创建人: hongyichao
+* 创建时间: 2019/6/11 18:07:58
+* 修改人: 洪移潮
+* 修改时间:
+* 版本： @version 1.0
+=====================================================*/
+/// <summary>
+/// 此处尽量简单,因为包括请求内容长度确认,标头,语言之类,通过MQ去确认
+/// 后期如果需要可以添加
+/// </summary>
+namespace Framework.MQRPC.Transfers
+{
+    public class TransferContentHeaders
+    {
+        //
+        // 摘要:
+        //     获取或设置 MQ 响应上的 Content-MD5 内容标头值。
+        //
+        // 返回结果:
+        //     MQ 响应上的 Content-MD5 内容标头值。
+        public byte[] ContentMD5 { get; set; }
+        //
+        // 摘要:
+        //     获取或设置 MQ 响应上的 Content-Location 内容标头值。
+        //
+        // 返回结果:
+        //     MQ 响应上的 Content-Location 内容标头值。
+        public Uri ContentLocation { get; set; }
+    }
+}
